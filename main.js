c0cb5e97300a16aad90d6bfea12e028230850390
Ekaterina <extra33333@mail.ru>
2019-02-08 00:06:14 +0300

    accordeon JS and slider items added

--- main.js
+++ main.js
@@ -9,3 +9,34 @@ const closeMenu=document.querySelector('.close__menu');
 closeMenu.addEventListener('click', function () {
   menu.style.display='none';
 });
+
+//аккордеон
+// js-team__item
+const items = document.querySelectorAll('.team__item');
+
+for (item of items) {
+
+  item.addEventListener('click', function () {
+    const curItem = event.currentTarget;
+    const teamText = curItem.querySelector('.team__text');
+    const textBlock = teamText.firstElementChild;
+    const reqHeight = textBlock.getBoundingClientRect().height;
+    
+    if (curItem.classList.contains('js-team__item')) {//открыт
+      
+      curItem.classList.remove('js-team__item');
+      teamText.style.height = 0;
+
+    } else {
+        Array.from(items).forEach(function(element) {
+        element.classList.remove('js-team__item');
+        element.querySelector('.team__text').style.height = 0;
+      });
+
+      curItem.classList.add('js-team__item');
+      teamText.style.height = `${reqHeight}px`;
+    }
+  });
+}
+
+//слайдер
