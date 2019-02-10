3034958846be568a85e239b9eb372269f5dae98e
Ekaterina <extra33333@mail.ru>
2019-02-10 18:44:53 +0300

    accordeon carte

---
+++ carte__acc.js
@@ -0,0 +1,28 @@
+const carteItems = document.querySelectorAll('.carte__item');
+
+for (eachItem of carteItems) {
+
+  eachItem.addEventListener('click', function () {
+    const curItem = event.currentTarget;
+    const carteDesc = curItem.querySelector('.carte__desc');
+    const carteAppear = carteDesc.firstElementChild;
+    const reqWidth = carteAppear.getBoundingClientRect.width;
+
+    if (curItem.classList.contains('js-carte__item')) {//открыт
+
+      curItem.classList.remove('js-carte__item');
+      carteDesc.style.width=git0;
+
+    } 
+    // else {
+    //     Array.from(carteItems).forEach(function(element) {
+    //     element.classList.remove('js-carte__item');
+    //     element.querySelector('.carte__appear').style.width = 0;
+    //   });
+    // }
+
+      curItem.classList.add('js-carte__item');
+      carteDesc.style.width=`${reqWidth}px`;
+  
+  });
+}
