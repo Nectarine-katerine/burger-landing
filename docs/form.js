45a2f4ab643eed849c03db4438e7cb29a4bc9d12
Ekaterina <45520757+Nectarine-katerine@users.noreply.github.com>
2019-02-10 23:58:53 +0300

    Add files via upload

---
+++ docs/form.js
@@ -0,0 +1,17 @@
+const phone = document.querySelector('#phone');
+
+phone.addEventListener('keydown', function() {
+
+  let isDigit = false;
+  let isControl=false;
+
+  if(event.key >=0 || event.key <=9) {
+    isDigit=true;
+  };
+  if(event.key == 'ArrowLeft' || event.key=='ArrowRight' || event.key=='Backspace' || event.key==='Tab') {
+    isControl=true;
+  };
+  if(!isDigit && !isControl) {
+    event.preventDefault();
+  };
+});
