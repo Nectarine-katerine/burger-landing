34847e61b75e758e6cd9a821a11a33b01b01ebd3
Ekaterina <extra33333@mail.ru>
2019-02-10 23:43:59 +0300

    carte accordeon ready

--- carte__acc.js
+++ carte__acc.js
@@ -1,27 +1,24 @@
 const carteItems = document.querySelectorAll('.carte__item');
 
-for (eachItem of carteItems) {
+for (var eachItem of carteItems) {
 
-  eachItem.addEventListener('click', function () {
+  eachItem.addEventListener('click', function(event) {
     const curItem = event.currentTarget;
     const carteDesc = curItem.querySelector('.carte__desc');
-    const carteAppear = carteDesc.firstElementChild;
-    const reqWidth = carteAppear.getBoundingClientRect.width;
-
     if (curItem.classList.contains('js-carte__item')) {//открыт
 
       curItem.classList.remove('js-carte__item');
       carteDesc.style.width=0;
-      
-    } else {
-      Array.from(carteItems).forEach(function (element) {
+    
+    } 
+    else {
+      Array.from(carteItems).forEach(function(element) {
         element.classList.remove('js-carte__item');
-        element.querySelector('.carte__appear').style.width=0;
+        element.querySelector('.carte__desc').style.width=0;
       });
-    }
 
     curItem.classList.add('js-carte__item');
-    carteDesc.style.width = `${reqWidth}px`;
-
+    carteDesc.style.width = 'initial';
+    }
   });
 }
