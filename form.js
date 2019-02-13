5198e358cd6bf3a705c9b4d6284cee2c0a216767
Ekaterina <extra33333@mail.ru>
2019-02-13 18:18:10 +0300

    form working

--- form.js
+++ form.js
@@ -15,3 +15,43 @@ phone.addEventListener('keydown', function() {
     event.preventDefault();
   };
 });
+
+const myForm=document.querySelector('#myForm');
+const formButton=document.querySelector('#formButton');
+const error=document.querySelectorAll('.error');
+
+formButton.addEventListener('click', function(event){
+  event.preventDefault();
+  if (validateForm(myForm)) {
+    console.log('Всё ок!');
+  }
+});
+
+function validateForm(form) {
+  let valid = true;
+
+  if(!validateField(myForm.elements.name)) {
+    valid=false;
+  }
+  if(!validateField(myForm.elements.phone)) {
+    valid=false;
+  }
+  if(!validateField(myForm.elements.comment)) {
+    valid=false;
+  }
+  return valid;
+};
+
+function validateField(field) {
+  for (var eachError of error) {
+    if (field.checkValidity(true)) {
+      eachError.textContent = '';
+      return field.checkValidity();
+    } else if (!field.checkValidity(false)){
+      eachError.textContent = field.validationMessage;
+    }
+  }
+  // if (!field.checkValidity()) {
+  // error.textContent=field.validationMessage;
+  // return field.checkValidity();
+}
