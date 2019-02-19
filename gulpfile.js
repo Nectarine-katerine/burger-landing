2161db4e5deeae60eb3da957c801873477ef3756
Ekaterina <extra33333@mail.ru>
2019-02-19 20:39:51 +0300

    npm

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,18 @@ var gulp = require('gulp'),
     concatCss = require('gulp-concat-css'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify'),
+    autoprefixer = require('gulp-autoprefixer'),
     minifyCSS = require('gulp-minify-css');
  
 gulp.task('default', function () {
   return gulp.src('css/*.css')
-    .pipe(concatCss('bundle.css'))
+    .pipe(concatCss('main.css'))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+  })
     .pipe(minifyCSS())
-    .pipe(rename('bundle.min.css'))
+    .pipe(rename('main.min.css'))
     .pipe(gulp.dest('app/'))
     .pipe(notify('Done!'));
 });
