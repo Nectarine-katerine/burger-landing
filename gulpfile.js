8051df2484730186d0bb20e8725cf7a118daa84b
Ekaterina <extra33333@mail.ru>
2019-02-20 22:15:37 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,13 @@
-var gulp = require('gulp'),
-    concatCss = require('gulp-concat-css'),
-    rename = require('gulp-rename'),
-    notify = require('gulp-notify'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifyCSS = require('gulp-minify-css');
- 
-gulp.task('default', function () {
-  return gulp.src('css/*.css')
-    .pipe(concatCss('main.css'))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-  })
-    .pipe(minifyCSS())
-    .pipe(rename('main.min.css'))
-    .pipe(gulp.dest('app/'))
-    .pipe(notify('Done!'));
-});
+const {src, dest, task} = require('gulp');
+const rm = require( 'gulp-rm' );
 
-gulp.task('watch', function(){
-  gulp.watch('css/*.css', ['default'])
+task( 'clean:tmp', function() {
+  return gulp.src( 'app/tmp/**/*', { read: false })
+    .pipe( rm() )
 })
+
+function copy(){
+  return src('src/css/**/*.scss').pipe(dest('dist'))
+}
+
+exports.copy = copy
