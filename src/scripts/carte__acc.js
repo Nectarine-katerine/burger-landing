e16668d39180df79b79f90bc79c561d0b27c0018
Ekaterina <extra33333@mail.ru>
2019-02-25 22:00:40 +0300

    slider carte ready

--- src/scripts/carte__acc.js
+++ src/scripts/carte__acc.js
@@ -1,13 +1,13 @@
 (function () {
   let verticalAcco = () => {
-    let calculateWidth = () => {
-      let windowWidth = window.innerWidth;
-      let titles = document.querySelectorAll('.carte__title');
-      let title = document.querySelector('.carte__title');
-      let titlesWidth = parseFloat(getComputedStyle(title).width);
-      let reqWidth = windowWidth - titlesWidth * titles.length;
-      return reqWidth > 550 ? 550 : reqWidth;
-    }
+    // let calculateWidth = () => {
+    //   let windowWidth = window.innerWidth;
+    //   let titles = document.querySelectorAll('.carte__title');
+    //   let title = document.querySelector('.carte__title');
+    //   let titlesWidth = parseFloat(getComputedStyle(title).width);
+    //   let reqWidth = windowWidth - titlesWidth * titles.length;
+    //   return reqWidth > 550 ? 550 : reqWidth;
+    // }
 
 
     let oMenulink = document.querySelectorAll('.carte__title');
@@ -28,8 +28,20 @@
           let currentPerson = e.target.closest(".carte__item");
           currentPerson.classList.add("js-carte__item");
 
-          let currentPersonInfo = currentPerson.querySelector(".menu__desc");
-          currentPersonInfo.style.width = calculateWidth() + "px";
+          currentPerson.lastElementChild.style.width = "100%"; 
+        }
+
+        const closeCarte = document.querySelectorAll('.close__carte');
+
+        for(var i=0; i<closeCarte.length; i++) {
+          closeCarte[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            const currentClose = e.target.closest(".js-carte__item");
+            currentClose.classList.remove('js-carte__item');
+
+            const curDes = e.target.closest(".carte__desc");
+            curDes.style.width = "0px";
+          })
         }
 
       })
